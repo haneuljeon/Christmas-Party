f0b436ee8796f8b268901cf1f72a0c593944d13a
HANEUL <jsky6766@gmail.com>
2024-11-19 14:03:13 +0900

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("enter-button").addEventListener("click", function() {
+    // 사진과 버튼을 숨깁니다
+    document.getElementById("welcome-section").style.display = "none";
+    
+    // 결혼식 정보를 보여줍니다
+    document.getElementById("wedding-info").style.display = "block";
+});
