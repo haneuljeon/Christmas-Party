7bb3aceaf757b4a7be9a5ffdde2905685072d235
HANEUL <jsky6766@gmail.com>
2024-11-28 13:16:34 +0900

    1

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById("enter-button").addEventListener("click", function() {
-    // 사진과 버튼을 숨깁니다
+
     document.getElementById("welcome-section").style.display = "none";
     
-    // 결혼식 정보를 보여줍니다
+
     document.getElementById("wedding-info").style.display = "block";
 });
 
