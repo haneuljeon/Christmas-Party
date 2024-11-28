982f6d2563dc9e17fb37cadfcdf489ffb73fc186
HANEUL <jsky6766@gmail.com>
2024-11-28 10:02:13 +0900

    snow

--- script.js
+++ script.js
@@ -20,3 +20,24 @@ document.addEventListener('DOMContentLoaded', function () {
     },
   });
 });
+
+function createSnowflakes(){
+  const snowflakeCount = 10;
+  const body = document.body;
+
+  for (let i =0; i < snowflakeCount; i++){
+    const snowflake = document.createElement('div');
+    snowflake.className = 'snowflake';
+    snowflake.style.left = Math.random() * 100 +'vw';
+    snowflake.style.animationDuration = Math.random() * 10 + 20 + 's';
+    snowflake.style.fontSize = Math.random() * 10 + 10 + 'px'; 
+    snowflake.innerHTML = '❄';
+    body.appendChild(snowflake);
+
+    setTimeout(() => snowflake.remove(), 10000);
+  }
+}
+
+window.onload = function () {
+  setInterval(createSnowflakes, 1000); 
+};
